c68797e215cd2ee6ede329980feaa2cacce8060c
Shahzad-Official <shahzad.official.90@gmail.com>
2023-12-11 02:13:43 +0500

    updated

--- src/Schemas/OrgnizationSchema.tsx
+++ src/Schemas/OrgnizationSchema.tsx
@@ -14,22 +14,21 @@ function OrgnizationSchema() {
           logo: "https://youtube-downloaders.com/logo.png",
           description:
             "Unlock a world of free entertainment and knowledge with our YouTube downloader. Download videos, shorts, and audio effortlessly in various resolutions.",
-          sameAs: [
-            "https://twitter.com/YourTwitterHandle",
-            "https://www.facebook.com/YourFacebookPage",
-            "https://www.linkedin.com/in/yourlinkedinprofile",
+          contactPoint: [
+            {
+              "@type": "ContactPoint",
+              telephone: "+923081888686",
+              contactType: "customer support",
+            },
           ],
-        })}
-      </script>
-
-      <script type="application/ld+json">
-        {JSON.stringify({
-          "@context": "https://schema.org",
-          "@type": "WebPage",
-          url: "https://youtube-downloaders.com",
-          name: "YouTube Downloaders",
-          description:
-            "Unlock a world of free entertainment and knowledge with our YouTube downloader.",
+          address: {
+            "@type": "PostalAddress",
+            streetAddress: "Main City",
+            addressLocality: "Multan",
+            addressRegion: "Punjab",
+            postalCode: "60000",
+            addressCountry: "Pakistan",
+          },
         })}
       </script>
     </Helmet>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import HomePage from "@/components/Home/HomePage";
 import FAQsSchema from "@/Schemas/FAQsSchema";
 import OrgnizationSchema from "@/Schemas/OrgnizationSchema";
+import HomePage from "@/components/Home/HomePage";
 
 export default function Home() {
   return (
