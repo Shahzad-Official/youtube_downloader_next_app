c96b46d11ce8198e9ac71f032972c07719920c2a
Shahzad-Official <shahzad.official.90@gmail.com>
2023-12-12 12:37:18 +0500

    updated

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,10 +2,8 @@ import React, { ReactNode } from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Drawer from "./Drawer/Drawer";
-import Helmet from "react-helmet";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Head from "next/head";
 import GTM from "@/utils/GTM";
 
 interface LayoutProps {
