81993791e6b3f47abd8167574e9c6ad122b3ac40
Shahzad-Official <shahzad.official.90@gmail.com>
2023-12-12 12:37:31 +0500

    updated

--- src/utils/GTM.tsx
+++ src/utils/GTM.tsx
@@ -5,14 +5,18 @@ import Helmet from "react-helmet";
 export default function GTM() {
   return (
     <>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZKGYZD6BHP"></Script>
-    <Script id="google-analytics">
-      {`
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZKGYZD6BHP"
+      ></Script>
+      <Script id="google-analytics">
+        {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-ZKGYZD6BHP');
       `}
-    </Script>
-  </>
-  )}
+      </Script>
+    </>
+  );
+}
